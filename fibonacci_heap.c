dd4f38617e5a4928551517fbbdae5da905d5e2b0
Rodion Efremov <rodion.efremov@helsinki.fi>
2016-01-26 14:59:21 +0200

    Working on consolidation.

--- fibonacci_heap.c
+++ fibonacci_heap.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-static const double PHI = 1.618;
+static const double LOG_PHI = 0.4813;
 static const size_t DEFAULT_NODE_ARRAY_CAPACITY = 16;
 
 typedef struct fibonacci_heap_node_t {
@@ -107,27 +107,33 @@ bool fibonacci_heap_t_add(fibonacci_heap_t* p_heap,
                           void* p_element, 
                           void* p_priority) 
 {
+    fibonacci_heap_node_t* p_node;
     
-}
-
-void fibonacci_heap_t_free(fibonacci_heap_t* p_heap)
-{
-    if (!p_heap) 
+    if (!p_heap) return false;
+    if (unordered_map_t_contains_key(p_heap->p_node_map, 
+                                     p_element)) return false;
+    
+    p_node = fibonacci_heap_node_t_alloc(p_element, p_priority);
+    
+    if (!p_node) return false;
+    
+    if (p_heap->p_minimum_node)
     {
-        return;
-    }
+        p_node->p_left = p_heap->p_minimum_node;
+        p_node->p_right = p_heap->p_minimum_node->p_right;
+        p_heap->p_minimum_node->p_right = p_node;
+        p_node->p_right->p_left = p_node;
+        
+        if (p_heap->p_key_compare_function(p_priority, 
+                                           p_heap->p_minimum_node->p_priority)) 
+        {
+            p_heap->p_minimum_node = p_node;
+        }
+    } 
+    else p_heap->p_minimum_node = p_node;
     
-    if (p_heap->p_node_array) 
-    {
-        free(p_heap->p_node_array);
-    }
-    
-    if (p_heap->p_node_map)
-    {
-        unordered_map_t_free(p_heap->p_node_map);
-    }
-    
-    free(p_heap);
+    unordered_map_t_put(p_heap->p_node_map, p_element, p_node);
+    return true;
 }
 
 static bool check_array(fibonacci_heap_t* p_heap, size_t size)
@@ -150,3 +156,161 @@ static bool check_array(fibonacci_heap_t* p_heap, size_t size)
         return true;
     }
 }
+
+static void consolidate(fibonacci_heap_t* p_heap)
+{
+    size_t array_size = 
+            (size_t)(floor(log(unordered_map_t_size(p_heap)) / LOG_PHI)) + 1;
+    size_t number_of_roots;
+    size_t degree;
+    size_t i;
+    fibonacci_heap_node_t* x;
+    fibonacci_heap_node_t* y;
+    fibonacci_heap_node_t* tmp;
+    fibonacci_heap_node_t* next;
+    
+    check_array(p_heap, array_size);
+    
+    /* Set the internal node array components to NULL. */
+    memset(p_heap->p_node_array, 
+           0, 
+           array_size * sizeof(fibonacci_heap_node_t*));
+    
+    number_of_roots = 0;
+    x = p_heap->p_minimum_node;
+    
+    if (x) 
+    {
+        ++number_of_roots;
+        x = x->p_right;
+        
+        while (x != p_heap->p_minimum_node)
+        {
+            ++number_of_roots;
+            x = x->p_right;
+        }
+    }
+    
+    while (number_of_roots > 0) 
+    {
+        degree = x->degree;
+        next = x->p_right;
+        
+        while(true)
+        {
+            y = p_heap->p_node_array[degree];
+            
+            if (!y) break;
+            
+            if (p_heap->p_key_compare_function(x->p_priority, 
+                                               y->p_priority) > 0) 
+            {
+                tmp = y;
+                y = x;
+                x = tmp;
+            }
+            
+            link(y, x);
+            p_heap->p_node_array[degree] = NULL;
+            ++degree;
+        }
+        
+        p_heap->p_node_array[degree] = x;
+        x = next;
+        --number_of_roots;
+    }
+    
+    p_heap->p_minimum_node = NULL;
+    
+    for (i = 0; i < array_size; ++i) 
+    {
+        y = p_heap->p_node_array[i];
+        
+        if (!y) continue;
+        
+        if (p_heap->p_minimum_node) 
+        {
+            y->p_left->p_right = y->p_right;
+            y->p_right->p_left = y->p_left;
+            
+        }
+        else
+        {
+            p_heap->p_minimum_node = y;
+        }
+    }
+}
+
+void* fibonacci_heap_t_extract_min(fibonacci_heap_t* p_heap)
+{
+    fibonacci_heap_node_t* z;
+    fibonacci_heap_node_t* x;
+    fibonacci_heap_node_t* tmp_right;
+    void* p_ret;
+    size_t number_of_children;
+    
+    if (!p_heap) return NULL;
+    
+    z = p_heap->p_minimum_node;
+    
+    if (!z) return NULL; /* Heap is empty. */
+    
+    number_of_children = z->degree;
+    x = z->p_child;
+
+    while (number_of_children > 0) 
+    {
+        tmp_right = x->p_right;
+
+        x->p_left->p_right = x->p_right;
+        x->p_right->p_left = x->p_left;
+
+        x->p_left = p_heap->p_minimum_node;
+        x->p_right = p_heap->p_minimum_node->p_right;
+        p_heap->p_minimum_node->p_right = x;
+        x->p_right->p_left = x;
+
+        x->p_parent = NULL;
+        x = tmp_right;
+        --number_of_children;
+    }
+        
+    z->p_left->p_right = z->p_right;
+    z->p_right->p_left = z->p_left;
+    
+    p_ret = p_heap->p_minimum_node->p_element;
+    
+    if (z == z->p_right)
+    {
+        p_heap->p_minimum_node = NULL;
+    }
+    else 
+    {
+        p_heap->p_minimum_node = z->p_right;
+        consolidate(p_heap);
+    }
+    
+    unordered_map_t_remove(p_heap->p_node_map, p_ret);
+    return p_ret;
+}
+
+void fibonacci_heap_t_free(fibonacci_heap_t* p_heap)
+{
+    if (!p_heap) 
+    {
+        return;
+    }
+    
+    if (p_heap->p_node_array) 
+    {
+        free(p_heap->p_node_array);
+    }
+    
+    if (p_heap->p_node_map)
+    {
+        unordered_map_t_free(p_heap->p_node_map);
+    }
+    
+    free(p_heap);
+}
+
